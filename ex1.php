44fe6e79dc5777b34ab6e28f0f94f5123b3651ec
kaytonfroeschl <57248811+kaytonfroeschl@users.noreply.github.com>
2020-11-24 16:23:30 -0600

    Create ex1.php

---
+++ ex1.php
@@ -0,0 +1,19 @@
+<?php
+$rows = 100;
+$cols = 100;
+
+echo "<table border=\"1\">";
+
+        for ($r =1; $r <= $rows; $r++){
+
+            echo'<tr>';
+
+            for ($c = 1; $c <= $cols; $c++)
+                echo '<td>' .$c*$r.'</td>';
+           echo '</tr>'; // close tr tag here
+
+        }
+
+  echo"</table>";
+
+  ?>
