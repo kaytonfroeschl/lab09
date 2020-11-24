518bb9ff49723c0b470f46ba978c4d96d823f4cd
kaytonfroeschl <57248811+kaytonfroeschl@users.noreply.github.com>
2020-11-24 16:25:20 -0600

    Create customerBackend.php

---
+++ customerBackend.php
@@ -0,0 +1,10 @@
+<?php
+    echo "THANK YOU FOR YOUR ORDER!
+<br />";
+    $answer = $_POST['password'];
+    echo "Your Password: ".$answer."
+<br>";
+
+    echo "Overview of your order:
+<br />";
+?>
